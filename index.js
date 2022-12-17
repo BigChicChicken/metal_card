f0c965aabbb447db42b297af0b0f8b217fdbf978
Florent TEDESCO <tedesco.florent.1996@live.fr>
2022-12-18 00:42:05 +0100

    Add orientation event

--- index.js
+++ index.js
@@ -30,16 +30,34 @@ window.addEventListener('load', () => {
     const card = document.getElementById('Card');
 
     if (card) {
-        const tracker = MouseTracker((e) => {
-            card.style.setProperty('--x', calculatePositionningInPercentage(e.layerX, card.offsetWidth));
-            card.style.setProperty('--y', calculatePositionningInPercentage(e.layerY, card.offsetHeight));
-        });
+        const { offsetWidth, offsetHeight } = card; 
 
-        tracker.observe(card);
+        if (window.DeviceOrientationEvent) {
+            let firstReading = true;
+            let base = null;
+            let timer = null;
 
-        card.addEventListener('mouseleave', () => {
-            card.style.setProperty('--x', 0);
-            card.style.setProperty('--y', 0);
-        });
+            window.addEventListener("deviceorientation", ({ beta, gamma }) => {
+                if (firstReading) {
+                  firstReading = false;
+                  base = { beta, gamma };
+                }
+
+                card.style.setProperty('--x', calculatePositionningInPercentage((base.gamma - gamma) * offsetWidth * .02 + offsetWidth / 2, offsetWidth));
+                card.style.setProperty('--y', calculatePositionningInPercentage((base.beta - beta) * offsetHeight * .02 + offsetHeight / 2, offsetHeight));
+            });
+        } else {
+            const tracker = MouseTracker(({layerX, layerY}) => {
+                card.style.setProperty('--x', calculatePositionningInPercentage(layerX, offsetWidth));
+                card.style.setProperty('--y', calculatePositionningInPercentage(layerY, offsetHeight));
+            });
+    
+            tracker.observe(card);
+    
+            card.addEventListener('mouseleave', () => {
+                card.style.setProperty('--x', 0);
+                card.style.setProperty('--y', 0);
+            });
+        }
     }
 });
